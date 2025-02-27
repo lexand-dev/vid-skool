1186fe9a3f369b4057ff3279afb7f2f5945e87d1
alexander flores <alexfloresrayme@gmail.com>
2025-02-26 21:36:58 -0500

    refactor: update database connection and schema definitions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,20 @@
 import {
-  PgTable,
-  text,
-  timestamp,
-  uniqueIndex,
-  uuid
+	pgTable,
+	text,
+	timestamp,
+	uniqueIndex,
+	uuid
 } from "drizzle-orm/pg-core";
 
-export const users = PgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  email: text("clerk_id").unique().notNull(),
-  name: text("name").notNull(),
-  imageUrl: text("image_url").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().notNull()
-});
+export const users = pgTable(
+	"users",
+	{
+		id: uuid("id").primaryKey().defaultRandom(),
+		clerkId: text("clerk_id").unique().notNull(),
+		name: text("name").notNull(),
+		imageUrl: text("image_url").notNull(),
+		createdAt: timestamp("created_at").defaultNow().notNull(),
+		updatedAt: timestamp("updated_at").defaultNow().notNull()
+	},
+	(t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+);
