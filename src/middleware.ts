afcc7b0d681d5501728f06ce66a8e56ff1fbe962
alexander flores <alexfloresrayme@gmail.com>
2025-03-02 01:34:06 -0500

    feat: add route matcher for studio paths protect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([]);
+const isPublicRoute = createRouteMatcher([
+  "/studio(.*)",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (isPublicRoute(request)) {
