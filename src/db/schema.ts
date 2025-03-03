e69c1d01f14eddb0105d16f9da9620b7d6040c95
alexander flores <alexfloresrayme@gmail.com>
2025-03-03 01:21:27 -0500

    chore: format schema.ts for improved readability and consistency

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,48 +1,47 @@
 import {
-	pgTable,
-	text,
-	timestamp,
-	uniqueIndex,
-	uuid,
-	varchar
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar
 } from "drizzle-orm/pg-core";
 
 import { timestamps } from "./columns.helpers";
 
 export const users = pgTable(
-	"users",
-	{
-		id: uuid("id").primaryKey().defaultRandom(),
-		clerkId: varchar("clerk_id", { length: 255 }).unique().notNull(),
-		name: varchar("name", { length: 255 }).notNull(),
-		imageUrl: varchar("image_url", { length: 255 }).notNull(),
-		...timestamps
-	},
-	(t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: varchar("clerk_id", { length: 255 }).unique().notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    imageUrl: varchar("image_url", { length: 255 }).notNull(),
+    ...timestamps
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
 );
 
 export const categories = pgTable(
-	"categories",
-	{
-		id: uuid("id").primaryKey().defaultRandom(),
-		name: varchar("name", { length: 100 }).unique().notNull(),
-		...timestamps
-	},
-	(t) => [uniqueIndex("name_idx").on(t.name)]
+  "categories",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 100 }).unique().notNull(),
+    ...timestamps
+  },
+  (t) => [uniqueIndex("name_idx").on(t.name)]
 );
 
-export const videos = pgTable(
-	"videos",
-	{
-		id: uuid("id").primaryKey().defaultRandom(),
-		title: varchar("title", { length: 255 }).notNull(),
-		description: text("description"),
-		userId: uuid("user_id").references(() => users.id, {
-			onDelete: "cascade"
-		} ).notNull(),
-		categoryId: uuid("category_id").references(() => categories.id, {
-			onDelete: "set null"
-		}),
-		...timestamps
-	}
-);
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: varchar("title", { length: 255 }).notNull(),
+  description: text("description"),
+  userId: uuid("user_id")
+    .references(() => users.id, {
+      onDelete: "cascade"
+    })
+    .notNull(),
+  categoryId: uuid("category_id").references(() => categories.id, {
+    onDelete: "set null"
+  }),
+  ...timestamps
+});
