d170b4c7ffa09da5d53fdc4d56ceb07dd162a50f
alexander flores <alexfloresrayme@gmail.com>
2025-03-03 11:45:29 -0500

    feat: enhance VideoSection with error handling, loading state, and improved layout

--- src/modules/studio/ui/views/studio-view.tsx
+++ src/modules/studio/ui/views/studio-view.tsx
@@ -2,7 +2,15 @@ import { VideoSection } from "../sections/video-section";
 
 export const StudioView = () => {
   return (
-    <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1 className="text-2xl font-bold">Channel Content</h1>
+        <p className="text-xs text-muted-foreground">
+          Manage your channel content and videos here. You can upload, edit and
+          delete videos.
+        </p>
+      </div>
+
       <VideoSection />
     </div>
   );
