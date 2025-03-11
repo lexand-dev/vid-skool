72188f8fbf9c262e3f9880c3e62a96ec2f7f2a10
alexander flores <alexfloresrayme@gmail.com>
2025-03-11 10:39:49 -0500

    feat: integrate Mux video upload functionality in videosRouter

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import { mux } from "@/lib/mux";
 import { videos } from "@/db/schema";
 
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
@@ -6,6 +7,19 @@ import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
+
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+        static_renditions: [
+          {
+            resolution: "highest"
+          }
+        ]
+      },
+      cors_origin: "*" // TODO: Set this to the domain of your app in production
+    });
 
     const [video] = await db
       .insert(videos)
@@ -16,7 +30,8 @@ export const videosRouter = createTRPCRouter({
       .returning();
 
     return {
-      video: video
+      video: video,
+      url: upload.url
     };
   })
 });
