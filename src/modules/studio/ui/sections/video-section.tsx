5e8b32321060ad3ec1814ccc4ca9c94e7061fc3a
alexander flores <alexfloresrayme@gmail.com>
2025-04-05 10:50:43 -0500

    feat: add video thumbnail display and loading skeleton to video section

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -2,10 +2,13 @@
 
 import Link from "next/link";
 import { Suspense } from "react";
+import { format } from "date-fns"
 import { ErrorBoundary } from "react-error-boundary";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/constants";
+import { snakeCaseToTitle } from "@/lib/utils";
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import {
   Table,
@@ -15,16 +18,71 @@ import {
   TableHeader,
   TableRow
 } from "@/components/ui/table";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const VideoSection = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<VideoSectionSkeleton />}>
       <ErrorBoundary fallback={<div>Something went wrong</div>}>
         <VideoSectionSuspense />
       </ErrorBoundary>
     </Suspense>
   );
 };
+
+const VideoSectionSkeleton = () => {
+  return (
+    <div className="border-y">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="pl-6 w-[510px]">Video</TableHead>
+            <TableHead>Visibility</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Views</TableHead>
+            <TableHead className="text-right">Comments</TableHead>
+            <TableHead className="text-right pr-6">Likes</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <TableRow key={index} >
+              <TableCell className="pl-6">
+                <div className="flex items-center gap-4">
+                  <Skeleton className="h-20 w-36" />
+                  <div className="flex flex-col overflow-hidden gap-y-2">
+                    <Skeleton className="h-4 w-[100px]" />
+                    <Skeleton className="h-3 w-[150px]" />
+                  </div>
+                </div>
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-20" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-16" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-24" />
+              </TableCell>
+              <TableCell className="text-right">
+                <Skeleton className="h-4 w-12 ml-auto" />
+              </TableCell>
+              <TableCell className="text-right">
+                <Skeleton className="h-4 w-12 ml-auto" />
+              </TableCell>
+              <TableCell className="text-right pr-6">
+                <Skeleton className="h-4 w-12 ml-auto" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
 
 const VideoSectionSuspense = () => {
   const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
@@ -61,10 +119,40 @@ const VideoSectionSuspense = () => {
                   legacyBehavior
                 >
                   <TableRow className="cursor-pointer">
-                    <TableCell>{video.title}</TableCell>
-                    <TableCell>Visibility</TableCell>
-                    <TableCell>status</TableCell>
-                    <TableCell>date</TableCell>
+                    <TableCell>
+                      <div className="flex items-center gap-4">
+                        <div className="relative w-36 shrink-0 aspect-video">
+                          <VideoThumbnail
+                            title={video.title}
+                            duration={video.duration ?? 0}
+                            imageUrl={video.thumbnailUrl}
+                            previewUrl={video.previewUrl}
+                          />
+                        </div>
+                        <div className="flex flex-col overflow-hidden gap-y-2">
+                          <span className="text-sm line-clamp-1">{video.title}</span>
+                          <span className="text-xs text-muted-foreground line-clamp-1">{video.description || "No description"}</span>
+                        </div>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center">
+                        {video.visibility === "private" ? (
+                          <LockIcon className="size-4 mr-2" />
+                        ) : (
+                          <Globe2Icon className="size-4 mr-2" />
+                        )}
+                        {snakeCaseToTitle(video.visibility)}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex items-center">
+                        {snakeCaseToTitle(video.muxStatus || "error")}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      {format(video.createdAt, "MMM dd, yyyy")}
+                    </TableCell>
                     <TableCell className="text-right">views</TableCell>
                     <TableCell className="text-right">comments</TableCell>
                     <TableCell className="text-right pr-6">likes</TableCell>
