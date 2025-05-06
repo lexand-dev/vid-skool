e35c8ed342ec10e795b9155b877569819cbd7268
alexander flores <alexfloresrayme@gmail.com>
2025-05-06 08:04:10 -0500

    style: format code for consistency in StudioLayout component

--- src/modules/studio/ui/layouts/studio-layout.tsx
+++ src/modules/studio/ui/layouts/studio-layout.tsx
@@ -4,19 +4,19 @@ import { StudioNavbar } from "../components/studio-navbar";
 import { StudioSidebar } from "../components/studio-sidebar";
 
 interface StudioLayoutProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const StudioLayout = ({ children }: StudioLayoutProps) => {
-	return (
-		<SidebarProvider>
-			<div className="w-full">
-				<StudioNavbar />
-				<div className="pt-[4rem] min-h-screen flex">
-					<StudioSidebar />
-					<main className="flex-1 overflow-y-auto">{children}</main>
-				</div>
-			</div>
-		</SidebarProvider>
-	);
+  return (
+    <SidebarProvider>
+      <div className="w-full">
+        <StudioNavbar />
+        <div className="pt-[4rem] min-h-screen flex">
+          <StudioSidebar />
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
 };
