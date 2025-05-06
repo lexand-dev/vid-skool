5b617dbd61ed87605549409e5f3376a68e2370e9
alexander flores <alexfloresrayme@gmail.com>
2025-05-06 09:18:56 -0500

    feat: add generateThumbnail mutation to trigger thumbnail generation workflow

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -7,9 +7,20 @@ import { db } from "@/db";
 import { videos, videoUpdateSchema } from "@/db/schema";
 
 import { TRPCError } from "@trpc/server";
+import { workflow } from "@/lib/workflow";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 export const videosRouter = createTRPCRouter({
+  generateThumbnail: protectedProcedure
+    .mutation(async ({ ctx }) => {
+      const { id: userId } = ctx.user;
+      const { workflowRunId } = await workflow.trigger({
+        url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
+        body: { userId }
+      });
+
+      return workflowRunId;
+  }),
   restoreThumbnail: protectedProcedure
     .input(z.object({ id: z.string().uuid() }))
     .mutation(async ({ ctx, input }) => {
